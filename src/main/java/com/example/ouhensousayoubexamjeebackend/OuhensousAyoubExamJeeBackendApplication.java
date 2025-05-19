04a840b766678add3c1edce0d30b28d34695d14b
ayoub <ayoub.ouhen@gmail.com>
2025-05-19 08:58:13 +0100

    first commit

---
+++ src/main/java/com/example/ouhensousayoubexamjeebackend/OuhensousAyoubExamJeeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ouhensousayoubexamjeebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuhensousAyoubExamJeeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuhensousAyoubExamJeeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ouhensousayoubexamjeebackend/OuhensousAyoubExamJeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ouhensousayoubexamjeebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuhensousAyoubExamJeeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
